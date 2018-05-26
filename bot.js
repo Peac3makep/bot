d72bc790998b028c6e56d654f06522ad2961cb3c
Peac3makep <39616845+Peac3makep@users.noreply.github.com>
2018-05-26 12:16:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,41 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+
+client.login('NDQ5Mjg5Mjk4NjQzMjU1Mjk3.DeiqQw.7ZoMYAvDeEOZp1GnqVD7CJ-iJzc');
+
 var prefix = '!'
 
+
+
+// HELP
 client.on('message', message => {
     if(message.author === client.user) return;
     if(message.content.startsWith(prefix + 'help')) {
-        message.channel.sendMessage('@here Ввести ХЕЛП');
+        message.channel.sendMessage('Ввести ХЕЛП');
      }
 });
 
-client.login('NDQ5Mjg5Mjk4NjQzMjU1Mjk3.DeiqQw.7ZoMYAvDeEOZp1GnqVD7CJ-iJzc');
+
+var Players=0;
+var author;
+
+// REGISTER
+client.on('message', message => {
+    if(message.author === client.user) return;
+    if(message.content.startsWith(prefix + 'register '+ (author) ) && Players == 0 ) {
+    var Player1= author;
+    Players= 1;
+
+    message.channel.sendMessage('Вы успешно зарегистрировались на матч');
+     }
+     if(message.content.startsWith(prefix + 'register '+ (author) ) && Players == 1 ) {
+    var Player2= author;
+     Players= 2;
+     message.channel.sendMessage('Вы успешно зарегистрировались на матч');
+     }
+     if(message.content.startsWith(prefix + 'register '+ (author) ) && Players >= 2 ) {
+     message.channel.sendMessage('@here Превышено допустимое кол-во игроков, дождитесь окончания матча');
+     }
+
+});
